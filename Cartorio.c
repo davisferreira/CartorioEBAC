9ca7ee1e96eed81c576694a6c9cbfc1f3861be7d
Davi Silva Ferreira <151973987+davisferreira@users.noreply.github.com>
2024-02-22 22:20:32 -0300

    Opção de saida
    Incluso no sistema a opção de saida para o usuario.

--- Cartorio.c
+++ Cartorio.c
@@ -129,6 +129,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: "); //fim do menu
 	
 		//printf("Esse software foi produzido por Davi S Ferreira.\n");
@@ -150,6 +151,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
